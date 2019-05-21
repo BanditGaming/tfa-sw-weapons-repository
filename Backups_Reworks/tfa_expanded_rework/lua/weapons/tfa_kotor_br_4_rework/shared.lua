2e6dc9c8e9903ec56e9665de06a9c02da5945706
septosezero <35184029+septosezero@users.noreply.github.com>
2019-05-21 08:11:59 -0400

    Update shared.lua

--- Backups_Reworks/tfa_expanded_rework/lua/weapons/tfa_kotor_br_4_rework/shared.lua
+++ Backups_Reworks/tfa_expanded_rework/lua/weapons/tfa_kotor_br_4_rework/shared.lua
@@ -9,7 +9,7 @@ if ( CLIENT ) then
 	SWEP.SlotPos			= 3
 end
 SWEP.Base					= "tfa_3dscoped_base"
-SWEP.Category = "TFA Star Wars"
+SWEP.Category = "TFA Blaster Rifles"
 SWEP.Spawnable				= true
 SWEP.AdminSpawnable			= true
 SWEP.HoldType = "ar2"
@@ -27,7 +27,7 @@ SWEP.Weight					= 5
 SWEP.AutoSwitchTo			= false
 SWEP.AutoSwitchFrom			= false
 SWEP.Primary.Recoil			= 0.6
-SWEP.Primary.Damage			= 40
+SWEP.Primary.Damage			= 30
 SWEP.Primary.NumShots		= 1
 SWEP.Primary.Spread			= 0.0125
 SWEP.Primary.IronAccuracy = .001	-- Ironsight accuracy, should be the same for shotguns
