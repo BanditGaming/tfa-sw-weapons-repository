129543e6ee50f4a12eb05e6a4f64867523a70d1a
septosezero <35184029+septosezero@users.noreply.github.com>
2019-05-20 23:00:19 -0400

    Update shared.lua

--- Live Addons/servius_uploader/tfa_extended_live/lua/weapons/tfa_t21b/shared.lua
+++ Live Addons/servius_uploader/tfa_extended_live/lua/weapons/tfa_t21b/shared.lua
@@ -9,7 +9,7 @@ if ( CLIENT ) then
 	SWEP.SlotPos			= 3
 end
 SWEP.Base					= "tfa_3dscoped_base"
-SWEP.Category = "TFA Star Wars"
+SWEP.Category = "TFA Blaster Rifles"
 SWEP.Spawnable				= true
 SWEP.AdminSpawnable			= true
 SWEP.HoldType = "ar2"
@@ -27,9 +27,9 @@ SWEP.Weight					= 5
 SWEP.AutoSwitchTo			= false
 SWEP.AutoSwitchFrom			= false
 SWEP.Primary.Recoil			= 0.4
-SWEP.Primary.Damage			= 55
+SWEP.Primary.Damage			= 15
 SWEP.Primary.NumShots		= 1
-SWEP.Primary.Spread			= 0.0125
+SWEP.Primary.Spread			= 0.015
 SWEP.Primary.IronAccuracy = .001	-- Ironsight accuracy, should be the same for shotguns
 SWEP.Primary.ClipSize		= 20
 SWEP.Primary.RPM            = 100
