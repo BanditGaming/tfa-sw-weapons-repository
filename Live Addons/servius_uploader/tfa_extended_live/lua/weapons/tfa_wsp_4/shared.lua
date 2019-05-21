82fa41c6278a2146da1c16c08393b6014f695836
septosezero <35184029+septosezero@users.noreply.github.com>
2019-05-20 23:03:06 -0400

    Update shared.lua

--- Live Addons/servius_uploader/tfa_extended_live/lua/weapons/tfa_wsp_4/shared.lua
+++ Live Addons/servius_uploader/tfa_extended_live/lua/weapons/tfa_wsp_4/shared.lua
@@ -10,7 +10,7 @@ if ( CLIENT ) then
 end
 SWEP.HoldType				= "ar2"
 SWEP.Base					= "tfa_3dscoped_base"
-SWEP.Category = "TFA Star Wars"
+SWEP.Category = "TFA Sniper Rifles"
 SWEP.Spawnable				= true
 SWEP.AdminSpawnable			= true
 SWEP.HoldType = "ar2"
@@ -31,7 +31,7 @@ SWEP.Weight					= 5
 SWEP.AutoSwitchTo			= false
 SWEP.AutoSwitchFrom			= false
 SWEP.Primary.Recoil			= 0.5
-SWEP.Primary.Damage			= 65
+SWEP.Primary.Damage			= 55
 SWEP.Primary.NumShots		= 1
 SWEP.Primary.Spread			= 0.0125
 SWEP.Primary.IronAccuracy = .001	-- Ironsight accuracy, should be the same for shotguns
