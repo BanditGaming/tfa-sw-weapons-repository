1010f5a50dc74248e6889cf6c4fbe9c792a32a50
septosezero <35184029+septosezero@users.noreply.github.com>
2019-05-20 23:57:07 -0400

    Update shared.lua

--- Live Addons/servius_uploader/tfa_swrp_expanded_weapons_pack/lua/weapons/tfa_kotor_bp_5/shared.lua
+++ Live Addons/servius_uploader/tfa_swrp_expanded_weapons_pack/lua/weapons/tfa_kotor_bp_5/shared.lua
@@ -10,7 +10,7 @@ if ( CLIENT ) then
 end
 SWEP.HoldType				= "revolver"
 SWEP.Base					= "tfa_swsft_base_servius"
-SWEP.Category = "TFA Star Wars"
+SWEP.Category = "TFA Blaster Pistols"
 SWEP.Spawnable				= true
 SWEP.AdminSpawnable			= true
 SWEP.HoldType = "ar2"
@@ -39,7 +39,7 @@ SWEP.Weight					= 5
 SWEP.AutoSwitchTo			= false
 SWEP.AutoSwitchFrom			= false
 SWEP.Primary.Recoil			= 0.2
-SWEP.Primary.Damage			= 70
+SWEP.Primary.Damage			= 25
 SWEP.Primary.NumShots		= 1
 SWEP.Primary.Spread			= 0.0125
 SWEP.Primary.IronAccuracy   = .001	-- Ironsight accuracy, should be the same for shotguns
